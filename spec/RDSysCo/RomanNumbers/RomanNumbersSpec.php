f18447b9d1a1e1deada5c441aebb9d181fcdab44
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2017-08-09 15:29:27 +0430

    supports all numbers up to 1000 - maybe more

--- spec/RDSysCo/RomanNumbers/RomanNumbersSpec.php
+++ spec/RDSysCo/RomanNumbers/RomanNumbersSpec.php
@@ -13,7 +13,77 @@ class RomanNumbersSpec extends ObjectBehavior
         $this->shouldHaveType(RomanNumbers::class);
     }
 
-    function it_returns_I_for_1 () {
+    function it_returns_I_for_1()
+    {
         $this->convert(1)->shouldReturn("I");
     }
+
+    function it_returns_II_for_2() {
+        $this->convert(2)->shouldReturn("II");
+    }
+
+    function it_returns_III_for_3() {
+        $this->convert(3)->shouldReturn("III");
+    }
+
+    function it_returns_V_for_5() {
+        $this->convert(5)->shouldReturn("V");
+    }
+
+    function it_returns_VIII_for_8(){
+        $this->convert(8)->shouldReturn("VIII");
+    }
+
+    function it_returns_X_for_10(){
+        $this->convert(10)->shouldReturn("X");
+    }
+
+    function it_returns_XXVI_for_26() {
+        $this->convert(26)->shouldReturn("XXVI");
+    }
+
+    function it_returns_L_for_50() {
+        $this->convert(50)->shouldReturn("L");
+    }
+
+    function it_returns_IV_for_4() {
+        $this->convert(4)->shouldReturn("IV");
+    }
+
+    function it_returns_IX_for_9() {
+        $this->convert(9)->shouldReturn("IX");
+    }
+
+    function it_returns_C_for_100() {
+        $this->convert(100)->shouldReturn("C");
+    }
+
+    function it_returns_XL_for_40() {
+        $this->convert(40)->shouldReturn("XL");
+    }
+
+    function it_returns_XC_for_90() {
+        $this->convert(90)->shouldReturn("XC");
+    }
+
+    function it_returns_D_for_500() {
+        $this->convert(500)->shouldReturn("D");
+    }
+
+    function it_returns_CD_for_400() {
+        $this->convert(400)->shouldReturn("CD");
+    }
+
+    function it_returns_M_for_1000() {
+        $this->convert(1000)->shouldReturn("M");
+    }
+
+    function it_returns_CM_for_900() {
+        $this->convert(900)->shouldReturn("CM");
+    }
+
+    function it_returns_CMLXXXIV_for_984() {
+        $this->convert(984)->shouldReturn("CMLXXXIV");
+    }
+
 }
--- src/RDSysCo/RomanNumbers/RomanNumbers.php
+++ src/RDSysCo/RomanNumbers/RomanNumbers.php
@@ -2,10 +2,36 @@
 
 namespace RDSysCo\RomanNumbers;
 
+use Psr\Log\InvalidArgumentException;
+
 class RomanNumbers
 {
+    protected static $lookup = [
+            1000 => "M",
+            900 => "CM",
+            500 => "D",
+            400 => "CD",
+            100 => "C",
+            90  => "XC",
+            50  => "L",
+            40  => "XL",
+            10  => "X",
+            9   => "IX",
+            5   => "V",
+            4   => "IV",
+            1   => "I"
+        ];
+
     public function convert($number)
     {
-        return "I";
+        $converted = "";
+
+        foreach (self::$lookup as $factor => $char) {
+            for(; $number >= $factor; $number -= $factor){
+                $converted .= $char;
+            }
+        }
+
+        return $converted;
     }
 }
