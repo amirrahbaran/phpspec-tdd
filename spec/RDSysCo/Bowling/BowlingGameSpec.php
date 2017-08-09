c5d166c0e0efe328fed8738b8cb109da1242c390
Mehrdad Hedayati <mehrdad.hedayati@rdsysco.com>
2017-08-09 17:16:18 +0430

    bowling game added with tests

--- spec/RDSysCo/Bowling/BowlingGameSpec.php
+++ spec/RDSysCo/Bowling/BowlingGameSpec.php
@@ -12,4 +12,57 @@ class BowlingGameSpec extends ObjectBehavior
     {
         $this->shouldHaveType(BowlingGame::class);
     }
+
+    function it_tests_gutter_game(){
+        $this->rollMany(20, 0);
+        $this->score()->shouldReturn(0);
+    }
+
+    function it_tests_all_ones(){
+        $this->rollMany(20,1);
+        $this->score()->shouldReturn(20);
+    }
+
+    function it_tests_one_spare() {
+        $this->rollSpare();
+        $this->roll(3);
+        $this->rollMany(17, 0);
+        $this->score()->shouldReturn(16);
+    }
+
+    function it_tests_one_strike() {
+        $this->rollStrike();
+        $this->roll(4);
+        $this->roll(5);
+        $this->rollMany(17, 0);
+        $this->score()->shouldReturn(28);
+    }
+
+    function it_tests_perfect_game() {
+        $this->rollMany(12, 10);
+        $this->score()->shouldReturn(300);
+    }
+
+    /**
+     * @param $count
+     * @param $pins
+     */
+    private function rollMany($count, $pins)
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $this->roll($pins);
+        }
+    }
+
+    private function rollSpare()
+    {
+        $this->roll(5);
+        $this->roll(5);
+    }
+
+    private function rollStrike()
+    {
+        $this->roll(10);
+    }
+
 }
--- src/RDSysCo/Bowling/BowlingGame.php
+++ src/RDSysCo/Bowling/BowlingGame.php
@@ -4,4 +4,55 @@ namespace RDSysCo\Bowling;
 
 class BowlingGame
 {
+
+    private $rolls = [];
+
+    public function roll($pins)
+    {
+        $this->rolls[] = $pins;
+    }
+
+    public function score()
+    {
+       $score = 0;
+
+       $frameIndex = 0;
+       for($frame = 0; $frame < 10; $frame++) {
+           if($this->isStrike($frameIndex)) {
+               $score += 10 + $this->rolls[$frameIndex+1] + $this->rolls[$frameIndex+2];
+               $frameIndex += 1;
+           }
+           elseif($this->isSpare($frameIndex)) {
+               $score += 10 + $this->rolls[$frameIndex + 2];
+               $frameIndex += 2;
+           } else {
+               $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+               $frameIndex += 2;
+           }
+
+       }
+
+       return $score;
+    }
+
+    /**
+     * @param $frameIndex
+     *
+     * @return bool
+     */
+    private function isSpare($frameIndex): bool
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
+    }
+
+    /**
+     * @param $frameIndex
+     *
+     * @return bool
+     */
+    private function isStrike($frameIndex): bool
+    {
+        return $this->rolls[$frameIndex] == 10;
+    }
+
 }
